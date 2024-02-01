2f64e312b98e42ba3cccd117dd65770b0700c8c5
Ankit Shukla <99482402+Theshukla06@users.noreply.github.com>
2024-02-01 14:06:34 +0530

    Update settings.py

--- learning/settings.py
+++ learning/settings.py
@@ -83,9 +83,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'version': '3.44.2', 
-        },
     }
 }
 
