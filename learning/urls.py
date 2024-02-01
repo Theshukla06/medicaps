7c45efee501d9e545bd9f350d85be300503e738c
Ankit Shukla <99482402+Theshukla06@users.noreply.github.com>
2024-02-01 14:30:14 +0530

    Update urls.py

--- learning/urls.py
+++ learning/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('user/',include(('user.urls', 'user'), namespace='user')),
 ]
